0825ccda1c618e319aa628b440be4feb2fe29150
Denis Bagaev <Denis@DEN-NEWPC>
2020-05-17 22:29:23 +0400

    В процессе

--- Bdv.ScanData/App.xaml.cs
+++ Bdv.ScanData/App.xaml.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Windows;
 
 namespace Bdv.ScanData
@@ -20,6 +21,14 @@ namespace Bdv.ScanData
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+            var mutex = new Mutex(true, "Bdv.ScanData", out var onlyOne);
+            if (!onlyOne)
+            {
+                MessageBox.Show("Другой экземпляр приложения уже запущен", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                Shutdown();
+                return;
+            }
+
             var worker = Locator.Kernel.Get<IWorkerService>();
             var logger = Locator.Kernel.Get<ILogger>();
 
